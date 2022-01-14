f32dee6aa6841f1a75e4a4c783093361e570e1cc
CristhianBuiza <cbuiza@ucvvirtual.edu.pe>
2022-01-14 14:12:02 -0500

    Bar

--- navigation.js
+++ navigation.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+//iconos
+import { Icon } from 'react-native-elements';
+
 
 //Importa Screen
 import HomeScreen from './src/screens/HomeScreen';
 import FavoritesScreen from './src/screens/FavoritesScreen';
+
 const Tab = createMaterialBottomTabNavigator();
 function MyTabs() {
     return (
         <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions= {{
-            tabBarActiveTintColor: 'purple',
-        }}
-      >
+            initialRouteName="Home"
+            activeColor="#f9fc44"
+            inactiveColor="#ffffff"
+            barStyle={{ backgroundColor: 'tomato' }}
             
-        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarLabel:'Pokemons',}}/>
-        <Tab.Screen name="Favorites" component={FavoritesScreen}  options={{tabBarLabel:'Favoritos', }}/>
+        >
+            
+        <Tab.Screen name="home-alert" component={HomeScreen} options={{tabBarLabel:'Pokemons',}}/>
+        <Tab.Screen name="Favorites" component={FavoritesScreen}  options={{tabBarLabel:'Favoritos',  }}/>
       </Tab.Navigator>
     );
   }
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {store} from '../app/store';
+import PokemonList from '../components/PokemonList';
+import {Provider} from 'react-redux';
+
+export default function HomeScreen() {
+    return (
+        
+
+            
+             <Provider store={store}>
+        <View>
+        <PokemonList />
+        </View>
+        </Provider>
+    )
+}
